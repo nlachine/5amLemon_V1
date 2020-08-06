d4ae20071b0bfb7d4e18629906fd0674277147e3
Noah Lachine <noah.lachine@ontariotechu.net>
2020-08-06 16:31:28 -0400

    [MOD] Adjusted Colours for Testing

--- 5am-lemon/src/components/landingpage/banner/Banner.js
+++ 5am-lemon/src/components/landingpage/banner/Banner.js
@@ -12,36 +12,36 @@ import ImgThree from '../../../images/Lemons3.jpg';
 export default class Banner extends Component {
     render() {
         return (
-            <section className="slider grey lighten-4" id="home">
+            <section className="slider valign-wrapper grey lighten-4" id="home">
                 <ul className="slides">
                     <li>
                         <img src={ImgOne} alt="Occasion" />
                         <div className="caption center-align">
-                            <h2>Pick your occasion</h2>
-                            <h5 className="light grey-text text-lighten-3 hide-on-small-only">
+                            <h2 className="black-text">Pick your occasion</h2>
+                            <h5 className="light grey-text text-darken-3 hide-on-small-only">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla non nisi at vestibulum.
                             </h5>
-                            <Link to="!#" className="btn-floating btn-large yellow hoverable waves-effect"><i class="material-icons">keyboard_arrow_down</i></Link>
+                            <Link to="!#" className="btn-floating btn-large yellow hoverable waves-effect"><i class="material-icons black-text">keyboard_arrow_down</i></Link>
                         </div>
                     </li>
                     <li>
                         <img src={ImgTwo} alt="Pattern" />
                         <div className="caption center-align">
-                            <h2>Pick your pattern</h2>
-                            <h5 className="light grey-text text-lighten-3 hide-on-small-only">
+                            <h2 className="grey-text">Pick your pattern</h2>
+                            <h5 className="light grey-text text-darken-1 hide-on-small-only">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla non nisi at vestibulum.
                             </h5>
-                            <Link to="!#" className="btn btn-large yellow hoverable waves-effect">Get Started</Link>
+                            <Link to="!#" className="btn btn-large yellow hoverable waves-effect black-text">Get Started</Link>
                         </div>
                     </li>
                     <li>
                         <img src={ImgThree} alt="Gift" />
                         <div className="caption center-align">
-                            <h2>Select your gift</h2>
-                            <h5 className="light grey-text text-lighten-3 hide-on-small-only">
+                            <h2 className="black-text">Select your gift</h2>
+                            <h5 className="light black-text hide-on-small-only">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla non nisi at vestibulum.
                             </h5>
-                            <Link to="!#" className="btn btn-large yellow hoverable waves-effect">Get Started</Link>
+                            <Link to="!#" className="btn btn-large yellow hoverable waves-effect black-text">Get Started</Link>
                         </div>
                     </li>
                 </ul>
--- 5am-lemon/src/components/landingpage/occasions/Occasions.js
+++ 5am-lemon/src/components/landingpage/occasions/Occasions.js
@@ -4,74 +4,74 @@ export default class Occasions extends Component {
     render() {
         return (
             <div>
-                <section id="occasion" className="section section-icons grey lighten-4 center">
-                    <div className = "container">
+                <section id="occasion" className="section section-icons grey lighten-4 center scrollspy">
+                    <div className="container">
                         {/*---Title---*/}
                         <h4 className="center">Occasions</h4>
-                        
-                        <div className = "row">
+
+                        <div className="row">
                             {/*---Occasion 1---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">cake</i>
-                                    <h5>Birthday</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">cake</i>
+                                    <h5 className="flow-text center-align">Birthday</h5>
                                 </div>
                             </div>
 
                             {/*---Occasion 2---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">business</i>
-                                    <h5>Corporate</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">business</i>
+                                    <h5 className="flow-text center-align">Corporate</h5>
                                 </div>
                             </div>
 
                             {/*---Occasion 3---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">favorite</i>
-                                    <h5>Just Because</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">favorite</i>
+                                    <h5 className="flow-text center-align">Just Because</h5>
                                 </div>
                             </div>
 
                             {/*---Occasion 4---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">thumb_up</i>
-                                    <h5>Thank You</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">thumb_up</i>
+                                    <h5 className="flow-text center-align">Thank You</h5>
                                 </div>
                             </div>
                         </div>
-                        <div className = "row">
+                        <div className="row">
                             {/*---Occasion 1---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">school</i>
-                                    <h5>Graduation</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">school</i>
+                                    <h5 className="flow-text center-align">Graduation</h5>
                                 </div>
                             </div>
 
                             {/*---Occasion 2---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">emoji_events</i>
-                                    <h5>Congratulations</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">emoji_events</i>
+                                    <h5 className="flow-text center-align">Congratulations</h5>
                                 </div>
                             </div>
 
                             {/*---Occasion 3---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">sick</i>
-                                    <h5>Get Well</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">sick</i>
+                                    <h5 className="flow-text center-align">Get Well</h5>
                                 </div>
                             </div>
 
                             {/*---Occasion 4---*/}
-                            <div className= "col s6 m3">
-                                <div className = "card-panel">
-                                    <i className = "material-icons large yellow-text">child_friendly</i>
-                                    <h5>New Born</h5>
+                            <div className="col s6 m3">
+                                <div className="card-panel hoverable">
+                                    <i className="material-icons large yellow-text">child_friendly</i>
+                                    <h5 className="flow-text center-align">New Born</h5>
                                 </div>
                             </div>
                         </div>
