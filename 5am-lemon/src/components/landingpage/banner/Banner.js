c93b05d8d28c75dc08bfb26006ac4e57b3242adc
Noah Lachine <noah.lachine@ontariotechu.net>
2020-08-12 16:15:59 -0400

    [MOD] Removed slider images, repositioned banner text

--- 5am-lemon/src/components/landingpage/banner/Banner.js
+++ 5am-lemon/src/components/landingpage/banner/Banner.js
@@ -5,8 +5,6 @@ import '../../../../node_modules/materialize-css/dist/css/materialize.css';
 import './Banner.css';
 
 import ImgOne from '../../../images/Lemons1a.jpg';
-import ImgTwo from '../../../images/Lemons2a.jpg';
-import ImgThree from '../../../images/Lemons3a.jpg';
 
 
 
@@ -17,33 +15,15 @@ export default class Banner extends Component {
                 <ul className="slides">
                     <li>
                         <img src={ImgOne} alt="Occasion" />
-                        <div className="caption center-align">
-                            <h2 className="white-text">Pick your occasion</h2>
-                            <h5 className="light white-text hide-on-small-only">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla non nisi at vestibulum.
-                            </h5>
-                            <AnchorLink href="/occasion" className="btn-floating btn-large yellow hoverable waves-effect"><i class="material-icons black-text">keyboard_arrow_down</i></AnchorLink>
-                        </div>
-                    </li>
-                    <li>
-                        <img src={ImgTwo} alt="Pattern" />
-                        <div className="caption center-align">
-                            <h2 className="white-text">Pick your pattern</h2>
-                            <h5 className="light white-text hide-on-small-only">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla non nisi at vestibulum.
-                            </h5>
-                            <AnchorLink href="/occasion" className="btn btn-large yellow hoverable waves-effect black-text">Get Started</AnchorLink>
-                        </div>
-                    </li>
-                    <li>
-                        <img src={ImgThree} alt="Gift" />
-                        <div className="caption center-align">
-                            <h2 className="white-text">Select your gift</h2>
-                            <h5 className="light white-text hide-on-small-only">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla non nisi at vestibulum.
-                            </h5>
-                            <AnchorLink href="/occasion" className="btn btn-large yellow hoverable waves-effect black-text">Get Started</AnchorLink>
-                        </div>
+                            <div className="container">
+                                <div className="caption left-align">
+                                    <h2 className="white-text">Pick your occasion</h2>
+                                    <h4 className="light white-text hide-on-small-only">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla non nisi at vestibulum.
+                                    </h4>
+                                    <AnchorLink href="/occasion" className="btn btn-large yellow hoverable waves-effect black-text">Pick An Occasion</AnchorLink>
+                                </div>
+                            </div>
                     </li>
                 </ul>
             </section>
--- 5am-lemon/src/components/landingpage/navbar/Navbar.js
+++ 5am-lemon/src/components/landingpage/navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import './navbar.scss';
 import '../../../../node_modules/materialize-css/dist/css/materialize.css';
 import '../../../../node_modules/materialize-css/dist/js/materialize';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+
+
 
 const SmoothScroll = () => {
     return (
